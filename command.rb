7e8246776ebc9f6550590ab88ab4845f91152007
Grace Andrews <grace.e.andrews@gmail.com>
2015-12-14 03:21:39 -0800

    create execute file

---
+++ command.rb
@@ -0,0 +1,12 @@
+# lib/vagrant-ls/command.rb
+module Vagrant
+    module Ls
+        class Command < Vagrant.plugin('2', :command)
+            def execute
+                exec('VBoxManage list vms')
+                0
+            end
+        end
+    end
+
+end
