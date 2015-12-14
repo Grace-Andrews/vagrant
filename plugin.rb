29d59f2c780463106ec4c429371b1b43840559cc
Grace Andrews <grace.e.andrews@gmail.com>
2015-12-14 03:18:27 -0800

    create plugin class

---
+++ plugin.rb
@@ -0,0 +1,20 @@
+# lib/vagrant_plugin/plugin.rb
+module Vagrant
+
+    module Ls
+
+        class Plugin < Vagrant.plugin('2')
+          name "List"
+
+          description <<-DESC
+          This plugin 'ls' all of the vms running inside the machine
+          DESC
+
+          command 'ls' do
+            require_relative 'command'
+            Command
+          end
+
+        end
+    end
+end
